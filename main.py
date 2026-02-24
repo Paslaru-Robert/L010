3b6048e150142e6e8532d620759aa9b49bf527f2
Paslaru-Robert <robert-mihai.paslaru@student.tuiasi.ro>
2026-02-24 22:12:57 +0200

    cod eliminare semne punctuaie

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import string
+
 def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+text = read_file("input.txt")
+print("=== Text original ===")
+print(text)
+
+text = remove_punctuation(text)
+print("=== Fara punctuatie ===")
+print(text)def read_file(filename):
     with open(filename, 'r') as f:
         return f.read()
 
